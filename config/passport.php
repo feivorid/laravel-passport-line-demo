ec3880d3f3b951e5826e50c162f7412d7d7359f6
feivorid <feivorid@gmail.com>
2019-12-22 15:07:28 +0800

    regenetrate passport keys

--- config/passport.php
+++ config/passport.php
@@ -2,7 +2,7 @@
 
 return [
     'grant_type'    => env('OAUTH_GRANT_TYPE', 'password'),
-    'client_id'     => env('OAUTH_CLIENT_ID', 3),
-    'client_secret' => env('OAUTH_CLIENT_SECRET', '1mnnEvqMIYV5OrQVhvJo55YNh9itZh3ccbceANQJ'),
+    'client_id'     => env('OAUTH_CLIENT_ID', 1),
+    'client_secret' => env('OAUTH_CLIENT_SECRET', 'i2NX8d1f1mT5wTqetJ8aZDW5Xs4GdgOQHYdKxl05'),
     'scope'         => env('OAUTH_SCOPE', '*'),
 ];
