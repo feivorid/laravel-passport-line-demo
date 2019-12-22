a250e2071f7a87daf7cd1fb8067f84137ff54fe6
feivorid <feivorid@gmail.com>
2019-12-22 20:10:34 +0800

    update passport keys8

--- config/passport.php
+++ config/passport.php
@@ -2,7 +2,7 @@
 
 return [
     'grant_type'    => env('OAUTH_GRANT_TYPE', 'password'),
-    'client_id'     => env('OAUTH_CLIENT_ID', 1),
-    'client_secret' => env('OAUTH_CLIENT_SECRET', 'i2NX8d1f1mT5wTqetJ8aZDW5Xs4GdgOQHYdKxl05'),
+    'client_id'     => env('OAUTH_CLIENT_ID', 2),
+    'client_secret' => env('OAUTH_CLIENT_SECRET', 'pvX1zC8JVqDIhKuaCat2nkFIt9cOG0fIOtOLnlNI'),
     'scope'         => env('OAUTH_SCOPE', '*'),
 ];
