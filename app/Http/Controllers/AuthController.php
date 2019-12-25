f3eeba6a73122e3e009a27049999decf334b829b
feivorid <feivorid@gmail.com>
2019-12-25 21:16:50 +0800

    fix route7

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -170,8 +170,8 @@ class AuthController extends Controller
 
     public function lineCallback()
     {
-        dd(Socialite::driver('line'));
-//        $user =Socialite::driver('line')->user();
-//        dd($user);
+        dd(Socialite::with('line')->user());
+        //        $user =Socialite::driver('line')->user();
+        //        dd($user);
     }
 }
